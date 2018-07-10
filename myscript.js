5485e4896e5f5bbcf592e995672687c8323d45ea
Atsushi Oobata <oobata@tokitech.co.jp>
2018-07-10 16:22:11 +0900

    記述をif文からスイッチ文に変更

--- myscript.js
+++ myscript.js
@@ -5,7 +5,22 @@
 
     btn.addEventListener("click",function(){
         var n = Math.floor(Math.random() * 3);
-        if (n === 0) {
+
+    switch(n){
+        case 0:
+        this.textContent= "大吉"
+        break
+
+        case 1:
+        this.textContent= "中吉"
+        break
+
+        case 2:
+        this.textContent= "凶"
+        break
+    }
+
+    /*    if (n === 0) {
         this.textContent='大吉';
     }
     else if (n === 1)　{
@@ -13,7 +28,7 @@
     }
     else if (n === 2)　{
         this.textContent='凶';
-    }
+    }*/
         //this.textContent=n;
     });
     btn.addEventListener("mousedown",function(){
