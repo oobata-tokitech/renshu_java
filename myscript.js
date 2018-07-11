d431686c3605ff25e0e7dadb3a0b5442593b7a95
Atsushi Oobata <oobata@tokitech.co.jp>
2018-07-11 18:27:17 +0900

    1度しかクリックできないように仕様を変更

--- myscript.js
+++ myscript.js
@@ -4,26 +4,24 @@
     var btn = document.getElementById("btn");
 
     btn.addEventListener("click",function(){
-        //var results = [ '大吉', '中吉', '凶', '末吉'];
-        //var results = [ '大吉', '中吉', '凶', '末吉'];
-        //var n = Math.floor(Math.random() * 4);
-        //var n = Math.floor(Math.random() * results.length);
-        //this.textContent = results[n];
-        var n =Math.random();
-        if (n<0.05){
-            this.textContent="大吉"; //5%
-        }
-        else if (n<0.2){
-            this.textContent="中吉"; //15%
-        }
-        else{
-            this.textContent="凶"; //80%
-        }
-    });
+        var n = Math.floor(Math.random() * 3);
+        
+        if (n === 0) {
+        this.textContent='大吉';
+    }
+    else if (n === 1) {
+        this.textContent='中吉';
+    }
+    else if (n === 2) {
+        this.textContent='凶';
+    }
+        //this.textContent=n;
+    },{once:true});
+    //ボタンが押された時にはクラス名pushedがつく
     btn.addEventListener("mousedown",function(){
         this.className ="pushed";
-    });
+    },{once:true});
     btn.addEventListener("mouseup",function(){
         this.className ="";
-    });
+    },{once:true});
 })();
